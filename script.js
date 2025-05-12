9761a531503e39c643531c184c655f332314a040
Amal Deep <amaldeep98@gmail.com>
2025-05-12 22:39:23 +0530

    almost complete

---
+++ script.js
@@ -0,0 +1,16 @@
+var mode=document.getElementById("mode")
+mode.addEventListener("click",dmode)
+var x=1;
+
+function dmode(){
+    if(x==1){
+        document.body.style.backgroundColor="black"
+        document.getElementById("web").style.color="white"
+        x=0;
+    }else{
+        document.body.style.backgroundColor="white"
+        document.getElementById("web").style.color="black"
+       
+        x=1
+    }
+}
