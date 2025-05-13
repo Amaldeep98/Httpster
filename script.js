401b4162da8310c99fda4236d0b807f58857450b
Amal Deep <amaldeep98@gmail.com>
2025-05-13 10:51:59 +0530

    nav bar colour changed

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function dmode(){
     if(x==1){
         document.body.style.backgroundColor="black"
         document.getElementById("web").style.color="white"
-        document.getElementById("mode").style.color="white"
+        // document.getElementById("mode").style.color="white"
         x=0;
     }else{
         document.body.style.backgroundColor="white"
         document.getElementById("web").style.color="black"
-        document.getElementById("mode").style.color="black"
+        // document.getElementById("mode").style.color="black"
         x=1
     }
 }
