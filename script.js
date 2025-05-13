f3a8a34d66a8e6590acd69ab87f3230ac1fc4163
Amal Deep <amaldeep98@gmail.com>
2025-05-13 10:57:56 +0530

    js corrected

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ function dmode(){
     if(x==1){
         document.body.style.backgroundColor="black"
         document.getElementById("web").style.color="white"
-        // document.getElementById("mode").style.color="white"
+        document.getElementById("mode").style.backgroundColor="white"
+        document.getElementById("mode").style.borderColor="black"
         x=0;
     }else{
         document.body.style.backgroundColor="white"
         document.getElementById("web").style.color="black"
-        // document.getElementById("mode").style.color="black"
+         document.getElementById("mode").style.backgroundColor="black"
+         document.getElementById("mode").style.borderColor="white"
         x=1
     }
 }
